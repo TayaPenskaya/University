04518e84ec3fd472b24a1d4dcfe97424768077d4
tpenskaya <tayaserf@gmail.com>
2019-05-26 00:15:45 +0300

    Created properties for TextStatistics

--- Java/TextStatistics/src/Main.java
+++ Java/TextStatistics/src/Main.java
@@ -1,6 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        //файл, который хранит свойства нашего проекта
+
+        File file = new File(Main.class.getResource("ru.properties").getFile());
+
+        Properties properties = new Properties();
+        properties.load(new FileReader(file));
+        String st = properties.getProperty("statistics");
+        System.out.println(st);
     }
 }
