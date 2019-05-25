aa0c87aef8bbde2880405134590c9b3b050995a8
tpenskaya <tayaserf@gmail.com>
2019-05-25 23:47:09 +0300

    Created new project "TextStatistics"

---
+++ Java/TextStatistics/src/TextStatistics.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.util.Locale;
+
+public class TextStatistics {
+    Locale inLanguage;
+    Locale outLanguage;
+    File in;
+    File out;
+
+    public void calcStatistics(){
+
+    }
+}
